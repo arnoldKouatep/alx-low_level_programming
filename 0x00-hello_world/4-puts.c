1e06f6e13e57ebbe3dd441e4c77209f91a1f853a
arnoldkouatep <arnoldkouayepdjom.cm@gmail.com>
2022-03-19 11:06:06 -0700

    Review task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  *  * main - prints the phrase with proper grammar,
  *   * but the outcome is a piece of art,
  *    * Return: 0 if exited properly, non-zero otherwise
- *     */
+ */
 int main(void)
 {
-	   puts("\"Programming is like building a multilingual puzzle\n");
-	   return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
 
