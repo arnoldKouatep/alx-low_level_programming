ec964fa0dcfb08748c9001f322b6d4ed6a4e5212
arnoldkouatep <arnoldkouayepdjom.cm@gmail.com>
2022-03-19 10:42:28 -0700

    Optional task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
-
-int main()
+/**
+ *  * main - prints the phrase with proper grammar,
+ *   * but the outcome is a piece of art,
+ *    * Return: 0 if exited properly, non-zero otherwise
+ *     */
+int main(void)
 {
 	   puts("\"Programming is like building a multilingual puzzle\n");
-	      return 0;
+	   return (0);
 }
 
